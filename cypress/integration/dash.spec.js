12f444c084463af6ae5921223c9f7a346fd057b8
pdribeiro <pedro.r.franco90@gmail.com>
2022-07-18 16:11:42 -0300

    factories

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -2,54 +2,37 @@ import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
 import dash from '../support/pages/dash'
 
+import { customer, provider, appointment } from '../support/factories/dash'
+
+
+
 
 describe('dashboard', function () {
 
     context('quando o cliente faz um agendamento no app mobile', function () {
 
-        const data = {
-
-            customer: {
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycrue.com',
-                password: 'pwd123',
-                is_provider: false
-
-            },
-
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramon@televisa.com',
-                password: 'pwd123',
-                is_provider: true
-
-            },
-
-            appointmentHour:'14:00'
-
-        }
 
         before(function () {
-            cy.postUser(data.provider)
-            cy.postUser(data.customer)
+            cy.postUser(provider)
+            cy.postUser(customer)
 
-            cy.apiLogin(data.customer)
+            cy.apiLogin(customer)
             cy.log('Conseguimos pegar o token ' + Cypress.env('apiToken'))
 
-            cy.setProviderId(data.provider.email)
-            cy.createAppointment(data.appointmentHour)
+            cy.setProviderId(provider.email)
+            cy.createAppointment(appointment.hour)
 
         })
 
         it(' o mesmo deve ser exibido no dashboard', function () {
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
             dashPage.calendarShouldBeVisible()
             const day = Cypress.env('appointmentDay')
             dashPage.selecDay(day)
 
-            dash.appointmentShouldBe(data.customer, data.appointmentHour)
+            dash.appointmentShouldBe(customer,appointmentHour)
 
         })
     })
